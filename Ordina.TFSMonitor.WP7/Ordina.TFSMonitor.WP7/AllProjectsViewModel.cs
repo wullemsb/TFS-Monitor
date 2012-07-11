d34e8b12b3df0fbe9cb12b2fe7a0b3c1bc2ebe06
BaWu <wullemsb@gmail.com>
2012-07-11 16:33:46 +0200

    Implemented all projects

--- Ordina.TFSMonitor.WP7/Ordina.TFSMonitor.WP7/AllProjectsViewModel.cs
+++ Ordina.TFSMonitor.WP7/Ordina.TFSMonitor.WP7/AllProjectsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Services.Client;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,14 +10,53 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Caliburn.Micro;
+using Ordina.TFSMonitor.Model.Entities;
 
 namespace Ordina.TFSMonitor.WP7
 {
     public class AllProjectsViewModel:Screen
     {
-        public AllProjectsViewModel()
+        private readonly SettingsPageViewModel _settings;
+        private DataServiceCollection<Project> _collection; 
+
+        public AllProjectsViewModel(SettingsPageViewModel settings)
         {
+            _settings = settings;
             this.DisplayName = "all";
         }
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+            LoadAvailableProjects();
+        }
+
+        private void LoadAvailableProjects()
+        {
+            var uri = new Uri(_settings.ServiceUrl+"/Projects");
+            var context = IoC.Get<TFSDataServiceContext>();
+            _collection = new DataServiceCollection<Project>(context);
+            _collection.LoadCompleted += this.ItemsLoadCompleted;
+            _collection.LoadAsync(uri);
+        }
+
+        private void ItemsLoadCompleted(object sender, LoadCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                
+            }
+            else if (e.Error != null)
+            {
+               
+            }
+            else
+            {
+                AllProjects = new BindableCollection<Project>(_collection);
+                this.NotifyOfPropertyChange(()=> this.AllProjects);
+            }
+        }
+
+        public IObservableCollection<Project> AllProjects { get; set; }
     }
 }
