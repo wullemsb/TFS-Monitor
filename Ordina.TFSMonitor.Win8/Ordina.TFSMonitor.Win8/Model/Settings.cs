218abd25d488273a65667a1faaef8ca873df2ff6
Bart Wullems <wullemsb@gmail.com>
2012-08-19 10:36:20 +0200

    Switch to C# version of Win8 instead of WinJS

---
+++ Ordina.TFSMonitor.Win8/Ordina.TFSMonitor.Win8/Model/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordina.TFSMonitor.Win8.Model
+{
+    public class Settings
+    {
+        public string ServiceUrl { get; set; }
+        public string Domain { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Ordina.TFSMonitor.Win8/Ordina.TFSMonitor.Win8/Model/TFSDataServiceContext.partial.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using Ordina.TFSMonitor.Win8.Model;
+
+namespace Ordina.TFSMonitor.Model.Entities
+{
+    /// <summary>
+    /// There are no comments for TFSData in the schema.
+    /// </summary>
+    public partial class TFSDataServiceContext : global::System.Data.Services.Client.DataServiceContext
+    {
+        /// <summary>
+        /// Initialize a new TFSData object.
+        /// </summary>
+        public TFSDataServiceContext(Settings settings) :
+            this(new Uri(settings.ServiceUrl))
+        {
+            this.SendingRequest += (s, e) =>
+            {
+                var credentials = string.Format(@"{0}\{1}:{2}", settings.Domain, settings.User, settings.Password);
+                e.RequestHeaders["Authorization"] = string.Format(CultureInfo.InvariantCulture, "Basic {0}", Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(credentials)));
+            };
+        }
+    }
+}
